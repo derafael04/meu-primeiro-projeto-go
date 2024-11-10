f51720c36d82eece84d7e62b0f2eb9f556eea505
derafael04 <derafael04@gmail.com>
2024-11-10 15:30:46 -0300

    Add user creation functionality and routes

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,22 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(r *gin.Context) {}
+	RestErr "github.com/derafael04/meu-primeiro-projeto-go/src/configuration/rest_err"
+	"github.com/derafael04/meu-primeiro-projeto-go/src/controller/model/request"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(r *gin.Context) {
+
+	var userRequest request.UserRequest
+
+	if err := r.ShouldBindJSON(&userRequest); err != nil {
+		restErr := RestErr.NewBadRequestError(fmt.Sprintf("invalid json body: %s", err.Error()))
+
+		r.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(userRequest)
+}
